1339ceab4d787b0231bb8194f90b2c4f16838d94
Jaitovich Bisnaud Jimeneqz <jaitovichbidnaud@gmail.com>
2025-12-10 14:32:22 -0600

    Fix: Bypass SSL validation and correct MainActivity lifecycle

--- app/src/main/java/com/example/pokemonapp/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/pokemonapp/data/remote/RetrofitInstance.kt
@@ -6,9 +6,37 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
 
+    private fun getUnsafeOkHttpClient(): okhttp3.OkHttpClient {
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<javax.net.ssl.TrustManager>(
+                object : javax.net.ssl.X509TrustManager {
+                    override fun checkClientTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+                    override fun checkServerTrusted(chain: Array<java.security.cert.X509Certificate>, authType: String) {}
+                    override fun getAcceptedIssuers(): Array<java.security.cert.X509Certificate> = arrayOf()
+                }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = javax.net.ssl.SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory = sslContext.socketFactory
+
+            okhttp3.OkHttpClient.Builder()
+                .sslSocketFactory(sslSocketFactory, trustAllCerts[0] as javax.net.ssl.X509TrustManager)
+                .hostnameVerifier { _, _ -> true }
+                .build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+
     val api: PokeApi by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(getUnsafeOkHttpClient())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(PokeApi::class.java)
